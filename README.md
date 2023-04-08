464ee9701f0a165115711ff35002d123b839e6e5
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-04-08 19:57:35 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# odin-cv-project
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-column items-center justify-center">
+      test
+    </div>
+  );
+}
+
+export default App;
